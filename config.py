9494f30eb93efe07508f6e15a1a35a603a052c9c
Adam Budai <budai8adam@gmail.com>
2019-09-20 17:05:35 +0200

    Change in config file.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # -----------
 # EVALUATION
 evaluation_result_folder = "results/eval_results"
-evaluation_data = "/userhome/student/budai/data/cp_vm_rot_40_raw_final" #"eval_data"
+evaluation_data = "eval_data"
 roi_left_weights = "weights/roi_left_ckp.pt"
 roi_right_weights = "weights/roi_right_ckp.pt"
 cp_left_weights = "weights/cp_left_ckp.pt"
@@ -10,12 +10,12 @@ cp_right_weights = "weights/cp_right_ckp.pt"
 # -----------
 # ROI TRAIN
 roi_result_folder = "results/roi_results"
-roi_data = "/userhome/student/budai/data/roi_raw_leftright_train" #"roi_data"
+roi_data = "roi_data"
 
 # -----------
 # CP TRAIN
 cp_result_folder = "results/cp_results"
-cp_data = "/userhome/student/budai/data/cp_vm_rot_40_224_noscalecrop_train" #"cp_data"
+cp_data = "cp_data"
 
 # -----------
 side = "right" # "right" or "left"
